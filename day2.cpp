8ca8dd754b1ba26f247d5dd6c70339fe534f3fb1
mohit-pareek09 <prkmohit14@gmail.com>
2024-05-18 20:12:11 +0530

    Add files via upload

---
+++ day2.cpp
@@ -0,0 +1,217 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+
+// 1234
+// 1234
+// 1234
+// 1234
+
+// int i=1;
+// int j=1;
+// int n;
+// cin>>n;
+
+// while(i<n+1){
+//     while(j<n+1){
+//         cout<<j;
+//         j=j+1;
+
+//     }
+//     cout<<endl;
+//     j=1;
+//     i=i+1;
+// }
+
+
+
+// 321
+// 321
+// 321
+
+// int i=1;
+// int j=1;
+// int n;
+// cin>>n;
+
+// while(i<n+1){
+//     while(j<n+1){
+//         cout<<n+1-j;
+//         j=j+1;
+
+//     }
+//     cout<<endl;
+//     j=1;
+//     i=i+1;
+// }
+
+
+// 123
+// 456
+// 789
+
+// int i=1;
+// int j=1;
+// int k=0;
+// int n;
+// cin>>n;
+
+// while(i<n+1){
+//     while(j<n+1){
+//         cout<<" "<<j+k;
+//         j=j+1;
+
+//     }
+//     cout<<endl;
+//     k=k+n;
+//     j=1;
+//     i=i+1;
+// }
+
+
+// *
+// **
+// ***
+// ****
+
+
+// int i=1;
+// int j=1;
+// int n;
+// cin>>n;
+
+// while(i<n+1){
+//     while(j<n+1){
+//         if(j<=i){
+//             cout<<"*";
+//         }
+        
+//         j=j+1;
+
+//     }
+//     cout<<endl;
+//     j=1;
+//     i=i+1;
+//             }
+
+
+
+// 1
+// 22
+// 333
+// 4444
+
+
+// int i=1;
+// int j=1;
+// int n;
+// cin>>n;
+
+// while(i<n+1){
+//     while(j<n+1){
+//         if(j<=i){
+//             cout<<i;
+//         }
+        
+//         j=j+1;
+
+//     }
+//     cout<<endl;
+//     j=1;
+//     i=i+1;
+//             }
+
+
+
+// 1
+// 12
+// 123
+// 1234
+
+
+// int i=1;
+// int j=1;
+// int k=0;
+// int n;
+// cin>>n;
+
+// while(i<n+1){
+//     while(j<n+1){
+//         if(j<=i){
+//             cout<<" "<<j+k;
+//         }
+        
+//         j=j+1;
+
+//     }
+//     k=k+i;
+//     cout<<endl;
+//     j=1;
+//     i=i+1;
+//             }
+
+
+
+
+
+
+// 1
+// 21
+// 321
+// 4321
+
+
+// int i=1;
+// int j=1;
+
+// int n;
+// cin>>n;
+
+// while(i<n+1){
+//     while(j<n+1){
+//         if(j<=i){
+//             cout<<" "<< i+1-j;
+//         }
+        
+//         j=j+1;
+
+//     }
+//     cout<<endl;
+//     j=1;
+//     i=i+1;
+//             }
+
+
+
+
+
+
+// AAA
+// BBB
+// CCC
+
+
+int i=1;
+int j=1;
+int k =64;
+int n;
+cin>>n;
+
+while(i<n+1){
+    while(j<n+1){
+
+      char d =k+j;
+        cout<< d ;
+        j=j+1;
+
+    }
+    k=k+n;
+    cout<<endl;
+    j=1;
+    i=i+1;
+            }
+
+    
+    return 0;
+}
